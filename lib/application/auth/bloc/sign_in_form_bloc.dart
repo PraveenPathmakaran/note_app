a0147ea63111437765a9b6f21263f9a1c2d08eda
Praveen <praveenpathmakaran@gmail.com>
2023-11-09 01:19:14 +0530

    authentication bloc created

--- lib/application/auth/bloc/sign_in_form_bloc.dart
+++ lib/application/auth/bloc/sign_in_form_bloc.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:note_app/domain/auth/auth_failure.dart';
 import 'package:note_app/domain/auth/value_objects.dart';
